57c6620ca29f9363ee5b6580583aacdb7cbeb025
Oswaldo G <oswaldo.gg0520@gmail.com>
2022-05-03 13:07:30 -0500

    Initializing react app and checkin if API is working correctly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from 'react';
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/users')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setUsers([...data]);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello World</h1>
+      {users.length == 0 ? (
+        <></>
+      ) : (
+        users.map((u, i) => <p key={i}>{u.username}</p>)
+      )}
     </div>
   );
 }
